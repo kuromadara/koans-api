1bbc4c8e7cc03ff5981e0d7cf3be6023327ba084
kuromadara <dowarah.dayananda35@gmail.com>
2023-05-01 13:45:24 +0530

    secretkey env

--- koansAPI/settings.py
+++ koansAPI/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c1v7_7p+*v-9nzeqf0_tm_%rt#x+5fkkog=(=dwn5+=0i#aahy'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
